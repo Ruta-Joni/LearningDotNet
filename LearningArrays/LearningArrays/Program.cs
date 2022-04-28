9f7157f017b061f2128b42e947623862505bff7e
Ruta-Joni <ruta.jonikaite12@gmail.com>
2022-04-28 13:27:08 +0300

    Solution commit

---
+++ LearningArrays/LearningArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vardai = new string[] { "Jonas", "Petras", "Juozas", "Tomas", "Benas" };
+
+            for (int i = 0; i < vardai.Length; i++)
+            {
+                Console.WriteLine(vardai[i]);
+            }
+
+            int[] metai = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20 };
+
+            for (int i = metai.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(metai[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
